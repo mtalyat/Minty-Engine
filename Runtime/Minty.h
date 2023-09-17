43a0a8e1f8127ef39f2a874b1505153d55c83231
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-09-17 15:58:56 -0500

    Update Minty.h

--- Runtime/Minty.h
+++ Runtime/Minty.h
@@ -16,7 +16,7 @@ ONLY include files that somebody writing a game would use.
 #include "M_Console.h"
 #include "M_Event.h"
 #include "M_File.h"
-#include "M_GameEngine.h"
+#include "M_Engine.h"
 #include "M_InputMap.h"
 #include "M_Material.h"
 #include "M_Math.h"
