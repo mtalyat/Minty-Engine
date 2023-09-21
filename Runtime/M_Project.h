c9882a89f1a6f948380b29b04369b802d64a3a3f
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-09-20 20:48:26 -0500

    Project documentation

--- Runtime/M_Project.h
+++ Runtime/M_Project.h
@@ -5,18 +5,38 @@
 
 namespace minty
 {
+	/// <summary>
+	/// Holds information for a project, which will be ran in the engine.
+	/// </summary>
 	class Project
 	{
 	private:
+		// the base filepath of the project
 		std::filesystem::path const _base;
 
 	public:
+		/// <summary>
+		/// Creates a new interface into a project at the given path.
+		/// </summary>
+		/// <param name="path"></param>
 		Project(std::string const& path);
 
+		/// <summary>
+		/// Gets the base file path for this project.
+		/// </summary>
+		/// <returns></returns>
 		std::string const getBasePath() const;
 
+		/// <summary>
+		/// Gets the Assets folder path for this project.
+		/// </summary>
+		/// <returns></returns>
 		std::string const getAssetsPath() const;
 
+		/// <summary>
+		/// Gets the Build folder path for this project.
+		/// </summary>
+		/// <returns></returns>
 		std::string const getBuildPath() const;
 	};
 }
