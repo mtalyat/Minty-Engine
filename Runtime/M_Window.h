23e32380f3c88fc0207881630d407dd703bfe65e
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-09-20 21:04:48 -0500

    Window documentation

--- Runtime/M_Window.h
+++ Runtime/M_Window.h
@@ -23,6 +23,12 @@ namespace minty
 		bool _mouseOutOfBounds;
 
 	public:
+		/// <summary>
+		/// Creates a new Window.
+		/// </summary>
+		/// <param name="title">The title to be displayed on the Window.</param>
+		/// <param name="width">The width of the Window in pixels.</param>
+		/// <param name="height">The height of the Window in pixels.</param>
 		Window(std::string const& title, int const width, int const height);
 
 		~Window();
@@ -57,14 +63,22 @@ namespace minty
 		/// <returns>The raw window.</returns>
 		GLFWwindow* getRaw() const;
 
+		/// <summary>
+		/// Sets the currently active InputMap for this Window.
+		/// </summary>
+		/// <param name="inputMap"></param>
 		void setInput(InputMap const* const inputMap);
 	private:
+		// triggers a key in the input map
 		void triggerKey(Key const key, KeyAction const action, KeyModifiers const mods);
 
+		// triggers a button in the input map
 		void triggerButton(MouseButton const button, KeyAction const action, KeyModifiers const mods);
 
+		// triggers a scroll in the input map
 		void triggerScroll(float dx, float dy);
 
+		// triggers a move in the input map
 		void triggerCursor(float x, float y);
 
 		// window resize
