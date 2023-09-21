021ab35a588c54e2ed941d3d13293824e95b3fcf
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-09-21 13:48:07 -0500

    Update Project, now collects asset file paths

--- Runtime/M_Project.cpp
+++ Runtime/M_Project.cpp
@@ -1,25 +1,83 @@
 #include "pch.h"
 #include "M_Project.h"
+#include <vector>
 
 using namespace minty;
 
 Project::Project(std::string const& path)
 	: _base(std::filesystem::absolute(path))
 {
-
+	collectAssets();
 }
 
-std::string const Project::getBasePath() const
+Project::filepath const Project::getBasePath() const
 {
-	return _base.string();
+	return _base;
 }
 
-std::string const Project::getAssetsPath() const
+Project::filepath const Project::getAssetsPath() const
 {
-	return (_base / "Assets").string();
+	return (_base / "Assets");
 }
 
-std::string const Project::getBuildPath() const
+Project::filepath const Project::getBuildPath() const
 {
-	return (_base / "Build").string();
+	return (_base / "Build");
+}
+
+void minty::Project::collectAssets()
+{
+	// list of directories to collect from
+	std::vector<filepath> pathsToCollect;
+
+	// add base directory to get started
+	pathsToCollect.push_back(getAssetsPath());
+
+	filepath path;
+
+	// keep collecting while paths to collect has something in it
+	while (pathsToCollect.size())
+	{
+		// get last element
+		path = pathsToCollect.back();
+
+		// remove it
+		pathsToCollect.pop_back();
+
+		// search the path
+		for (const auto& entry : std::filesystem::directory_iterator(path))
+		{
+			filepath p = entry.path();
+
+			if (entry.is_directory())
+			{
+				// if another directory, we want to search it later
+				pathsToCollect.push_back(p);
+			}
+			else
+			{
+				// if a file, check the file type and add where necessary
+				std::string extension = path.extension().string();
+
+				// add to appropriate collection of file names
+				if (_headerExtensions.contains(extension))
+				{
+					_headers.emplace(path);
+				}
+				else if (_sourceExtensions.contains(extension))
+				{
+					_sources.emplace(path);
+				}
+				else if (_sceneExtensions.contains(extension))
+				{
+					_scenes.emplace(path);
+				}
+				else
+				{
+					// must be something else
+					_others.emplace(path);
+				}
+			}
+		}
+	}
 }
--- Runtime/M_Project.h
+++ Runtime/M_Project.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <filesystem>
+#include <set>
 
 namespace minty
 {
@@ -10,9 +11,26 @@ namespace minty
 	/// </summary>
 	class Project
 	{
+		typedef std::filesystem::path filepath;
+
 	private:
 		// the base filepath of the project
-		std::filesystem::path const _base;
+		filepath const _base;
+
+		// all of the header (.h) files in the Assets folder
+		std::set<filepath> _headers;
+		std::set<std::string> const _headerExtensions = { ".h" };
+
+		// all of the source (.cpp) files in the Assets folder
+		std::set<filepath> _sources;
+		std::set<std::string> const _sourceExtensions = { ".cpp" };
+
+		// all of the scene (.scene) files in the Assets folder
+		std::set<filepath> _scenes;
+		std::set<std::string> const _sceneExtensions = { ".scene" };
+
+		// everything else
+		std::set<filepath> _others;
 
 	public:
 		/// <summary>
@@ -25,18 +43,22 @@ namespace minty
 		/// Gets the base file path for this project.
 		/// </summary>
 		/// <returns></returns>
-		std::string const getBasePath() const;
+		filepath const getBasePath() const;
 
 		/// <summary>
 		/// Gets the Assets folder path for this project.
 		/// </summary>
 		/// <returns></returns>
-		std::string const getAssetsPath() const;
+		filepath const getAssetsPath() const;
 
 		/// <summary>
 		/// Gets the Build folder path for this project.
 		/// </summary>
 		/// <returns></returns>
-		std::string const getBuildPath() const;
+		filepath const getBuildPath() const;
+
+	private:
+		// searches through the project assets folder and finds all files of certain types
+		void collectAssets();
 	};
 }
