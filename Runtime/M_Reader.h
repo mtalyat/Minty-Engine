ae5b73063230b8b6f1cbbcbde3e9d3d60b6d0826
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-10-17 22:03:03 -0500

    Add serialize/deserialize for Vector and Quaternion

--- Runtime/M_Reader.h
+++ Runtime/M_Reader.h
@@ -3,6 +3,8 @@
 #include "M_Types.h"
 #include "M_Object.h"
 #include "M_SerializedNode.h"
+#include "M_Vector.h"
+#include "M_Quaternion.h"
 #include <map>
 #include <vector>
 
@@ -41,5 +43,19 @@ namespace minty
 		float read_float(std::string const& name, float const defaultValue = 0.0f) const;
 
 		byte read_byte(std::string const& name, byte const defaultValue = 0) const;
+
+		Vector2 read_vector2(std::string const& name, Vector2 const& defaultValue = Vector2()) const;
+
+		Vector3 read_vector3(std::string const& name, Vector3 const& defaultValue = Vector3()) const;
+
+		Vector4 read_vector4(std::string const& name, Vector4 const& defaultValue = Vector4()) const;
+
+		Vector2Int read_vector2int(std::string const& name, Vector2Int const& defaultValue = Vector2Int()) const;
+
+		Vector3Int read_vector3int(std::string const& name, Vector3Int const& defaultValue = Vector3Int()) const;
+
+		Vector4Int read_vector4int(std::string const& name, Vector4Int const& defaultValue = Vector4Int()) const;
+
+		Quaternion read_quaternion(std::string const& name, Quaternion const& defaultValue = Quaternion()) const;
 	};
 }
--- Runtime/M_Writer.cpp
+++ Runtime/M_Writer.cpp
@@ -51,6 +51,99 @@ void minty::Writer::write(std::string const& name, byte const value)
 	write(name, std::to_string(value));
 }
 
+void minty::Writer::write(std::string const& name, Vector2 const& value)
+{
+	SerializedNode node;
+
+	Writer writer(node);
+
+	writer.write("x", value.x);
+	writer.write("y", value.y);
+
+	write(name, node);
+}
+
+void minty::Writer::write(std::string const& name, Vector3 const& value)
+{
+	SerializedNode node;
+
+	Writer writer(node);
+
+	writer.write("x", value.x);
+	writer.write("y", value.y);
+	writer.write("z", value.z);
+
+	write(name, node);
+}
+
+void minty::Writer::write(std::string const& name, Vector4 const& value)
+{
+	SerializedNode node;
+
+	Writer writer(node);
+
+	writer.write("x", value.x);
+	writer.write("y", value.y);
+	writer.write("z", value.z);
+	writer.write("w", value.w);
+
+	write(name, node);
+}
+
+void minty::Writer::write(std::string const& name, Vector2Int const& value)
+{
+	SerializedNode node;
+
+	Writer writer(node);
+
+	writer.write("x", value.x);
+	writer.write("y", value.y);
+
+	write(name, node);
+}
+
+void minty::Writer::write(std::string const& name, Vector3Int const& value)
+{
+	SerializedNode node;
+
+	Writer writer(node);
+
+	writer.write("x", value.x);
+	writer.write("y", value.y);
+	writer.write("z", value.z);
+
+	write(name, node);
+}
+
+void minty::Writer::write(std::string const& name, Vector4Int const& value)
+{
+	SerializedNode node;
+
+	Writer writer(node);
+
+	writer.write("x", value.x);
+	writer.write("y", value.y);
+	writer.write("z", value.z);
+	writer.write("w", value.w);
+
+	write(name, node);
+}
+
+void minty::Writer::write(std::string const& name, Quaternion const& value)
+{
+	SerializedNode node;
+
+	Writer writer(node);
+
+	Vector3 angles = glm::eulerAngles(value);
+
+	writer.write("x", angles.x); // pitch
+	writer.write("y", angles.y); // yaw
+	writer.write("z", angles.z); // roll
+
+	write(name, node);
+}
+
 void minty::Writer::write(std::string const& name, std::string const& value, std::string const& defaultValue)
 {
 	if (value.compare(defaultValue))
@@ -81,5 +174,120 @@ void minty::Writer::write(std::string const& name, byte const value, byte const 
 	if (value != defaultValue)
 	{
 		write(name, std::to_string(value));
+	}
+}
+
+void minty::Writer::write(std::string const& name, Vector2 const& value, Vector2 const& defaultValue)
+{
+	if (value != defaultValue)
+	{
+		SerializedNode node;
+
+		Writer writer(node);
+
+		writer.write("x", value.x, defaultValue.x);
+		writer.write("y", value.y, defaultValue.y);
+
+		write(name, node);
+	}
+}
+
+void minty::Writer::write(std::string const& name, Vector3 const& value, Vector3 const& defaultValue)
+{
+	if (value != defaultValue)
+	{
+		SerializedNode node;
+
+		Writer writer(node);
+
+		writer.write("x", value.x, defaultValue.x);
+		writer.write("y", value.y, defaultValue.y);
+		writer.write("z", value.z, defaultValue.z);
+
+		write(name, node);
+	}
+}
+
+void minty::Writer::write(std::string const& name, Vector4 const& value, Vector4 const& defaultValue)
+{
+	if (value != defaultValue)
+	{
+		SerializedNode node;
+
+		Writer writer(node);
+
+		writer.write("x", value.x, defaultValue.x);
+		writer.write("y", value.y, defaultValue.y);
+		writer.write("z", value.z, defaultValue.z);
+		writer.write("w", value.w, defaultValue.w);
+
+		write(name, node);
+	}
+}
+
+void minty::Writer::write(std::string const& name, Vector2Int const& value, Vector2Int const& defaultValue)
+{
+	if (value != defaultValue)
+	{
+		SerializedNode node;
+
+		Writer writer(node);
+
+		writer.write("x", value.x, defaultValue.x);
+		writer.write("y", value.y, defaultValue.y);
+
+		write(name, node);
+	}
+}
+
+void minty::Writer::write(std::string const& name, Vector3Int const& value, Vector3Int const& defaultValue)
+{
+	if (value != defaultValue)
+	{
+		SerializedNode node;
+
+		Writer writer(node);
+
+		writer.write("x", value.x, defaultValue.x);
+		writer.write("y", value.y, defaultValue.y);
+		writer.write("z", value.z, defaultValue.z);
+
+		write(name, node);
+	}
+}
+
+void minty::Writer::write(std::string const& name, Vector4Int const& value, Vector4Int const& defaultValue)
+{
+	if (value != defaultValue)
+	{
+		SerializedNode node;
+
+		Writer writer(node);
+
+		writer.write("x", value.x, defaultValue.x);
+		writer.write("y", value.y, defaultValue.y);
+		writer.write("z", value.z, defaultValue.z);
+		writer.write("w", value.w, defaultValue.w);
+
+		write(name, node);
+	}
+}
+
+void minty::Writer::write(std::string const& name, Quaternion const& value, Quaternion const& defaultValue)
+{
+	if (value != defaultValue)
+	{
+		SerializedNode node;
+
+		Writer writer(node);
+
+		Vector3 angles = glm::eulerAngles(value);
+		Vector3 defaultAngles = glm::eulerAngles(value);
+
+		writer.write("x", angles.x, defaultAngles.x); // pitch
+		writer.write("y", angles.y, defaultAngles.y); // yaw
+		writer.write("z", angles.z, defaultAngles.z); // roll
+
+		write(name, node);
 	}
 }
