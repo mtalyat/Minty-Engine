57390b6bc556c9b9798f86d3c2dc4d8c20e2297d
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-10-19 10:00:20 -0500

    Update basic vertex to use normal instead of color

--- Runtime/M_Basic_Vertex.h
+++ Runtime/M_Basic_Vertex.h
@@ -6,8 +6,8 @@ namespace minty::basic
 {
 	struct Vertex
 	{
-		glm::vec3 pos;
-		glm::vec3 color;
-		glm::vec2 coord;
+		glm::vec3 pos; // position of vertex
+		glm::vec3 normal; // normal of vertex
+		glm::vec2 coord; // fragment coordinate of vertex
 	};
 }
