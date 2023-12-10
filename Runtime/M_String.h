4e2592053d2b18b22794b58d554c2cb11309a45d
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-12-10 02:37:44 -0600

    Add to_bytes and to_bits

--- Runtime/M_String.h
+++ Runtime/M_String.h
@@ -9,6 +9,10 @@ namespace minty::string
 
 	std::string to_upper(std::string const& string);
 
+	std::string to_bytes(void* const data, size_t const size);
+
+	std::string to_bits(void* const data, size_t const size);
+
 	bool equal_insensitive(std::string const& left, std::string const& right);
 
 	std::vector<std::string> split(std::string const& string);
