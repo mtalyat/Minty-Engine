2de99be065aa57a0a2a3e45314c82b9df2725a2d
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2023-12-28 15:57:57 -0600

    Fix error code coloring in editor

--- Projects/Tests/TestProject/Build/main.cpp
+++ Projects/Tests/TestProject/Build/main.cpp
@@ -1,5 +1,7 @@
-// 2023-12-27 22:21:10
+// 2023-12-28 21:56:05
 #include <Minty.h>
+#include <string>
+#include <format>
 #include <filesystem>
 #include "../Assets/Scripts/init.h"
 int main(int argc, char const* argv[]) {
@@ -7,8 +9,8 @@ int main(int argc, char const* argv[]) {
 	minty::console::log(std::filesystem::current_path().string());
 	minty::Info info("TestProject - HARD CODED NAME", 0, 0, 0);
 	minty::Runtime rt(info);
-	init(rt);
+	if(int code = init(rt)) minty::console::error(std::format("Failed to init program with error code {}.", code));
 	int result = rt.run();
-	destroy(rt);
+	if(int code = destroy(rt)) minty::console::error(std::format("Failed to destroy program with error code {}.", code));
 	return result;
 }
