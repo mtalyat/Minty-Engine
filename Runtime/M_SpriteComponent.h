cee708c364b276f0cfc4712e6d2a33f7fa0332c6
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2024-01-14 23:26:16 -0600

    Add SpriteComponent defaults

--- Runtime/M_SpriteComponent.h
+++ Runtime/M_SpriteComponent.h
@@ -14,17 +14,17 @@ namespace minty
 		/// <summary>
 		/// The ID of the Sprite to render.
 		/// </summary>
-		ID spriteId;
+		ID spriteId = ERROR_ID;
 
 		/// <summary>
 		/// The size of the Sprite in world space.
 		/// </summary>
-		Vector2 size;
+		Vector2 size = Vector2(1.0f, 1.0f);
 
 		/// <summary>
 		/// The order in which to render the Sprite, when it has equal distance to the Camera as another Sprite.
 		/// </summary>
-		int order;
+		int order = 0;
 
 		void serialize(Writer& writer) const override;
 		void deserialize(Reader const& reader) override;
