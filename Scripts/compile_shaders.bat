0f536731e9c1015b533346d5b98c4d9c84ff3ebb
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2024-06-15 12:51:31 -0500

    Update compile_shaders.bat

--- Scripts/compile_shaders.bat
+++ Scripts/compile_shaders.bat
@@ -4,6 +4,6 @@
 @REM C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe %~dp0sprite.frag -o %~dp0sprite_frag.spv
 @REM C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe %~dp0ui.vert -o %~dp0uivert.spv
 @REM C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe %~dp0ui.frag -o %~dp0uifrag.spv
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Projects/Tests/TestProject/Assets/WIP/ui.vert -o C:/Users/mitch/source/repos/Minty-Engine/Projects/Tests/TestProject/Assets/WIP/uiVert.spv
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Projects/Tests/TestProject/Assets/WIP/ui.frag -o C:/Users/mitch/source/repos/Minty-Engine/Projects/Tests/TestProject/Assets/WIP/uiFrag.spv
+C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/minty/TestProject/Assets/Fonts/defaultTextShader.vert -o C:/Users/mitch/source/minty/TestProject/Assets/Fonts/defaultTextShaderVert.spv
+C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/minty/TestProject/Assets/Fonts/defaultTextShader.frag -o C:/Users/mitch/source/minty/TestProject/Assets/Fonts/defaultTextShaderFrag.spv
 pause
