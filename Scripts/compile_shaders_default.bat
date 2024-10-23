044ffaa03e4e8b0af7c190ac4c8833b5e52d3c9a
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2024-10-22 23:21:13 -0500

    Update Scripts

--- Scripts/compile_shaders_default.bat
+++ Scripts/compile_shaders_default.bat
@@ -1,7 +1,9 @@
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultShader.vert -o C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultVert.spv
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultShader.frag -o C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultFrag.spv
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultSprite.vert -o C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultSpriteVert.spv
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultSprite.frag -o C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultSpriteFrag.spv
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultUi.vert -o C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultUiVert.spv
-C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultUi.frag -o C:/Users/mitch/source/repos/Minty-Engine/Data/default/Shader/defaultUiFrag.spv
+set compiler=C:/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe
+
+%compiler% ../Data/default/ShaderModule/GLSL/defaultShader.vert -o ../Data/default/ShaderModule/Compiled/defaultVert.spv
+%compiler% ../Data/default/ShaderModule/GLSL/defaultShader.frag -o ../Data/default/ShaderModule/Compiled/defaultFrag.spv
+%compiler% ../Data/default/ShaderModule/GLSL/defaultSprite.vert -o ../Data/default/ShaderModule/Compiled/defaultSpriteVert.spv
+%compiler% ../Data/default/ShaderModule/GLSL/defaultSprite.frag -o ../Data/default/ShaderModule/Compiled/defaultSpriteFrag.spv
+%compiler% ../Data/default/ShaderModule/GLSL/defaultUi.vert -o ../Data/default/ShaderModule/Compiled/defaultUiVert.spv
+%compiler% ../Data/default/ShaderModule/GLSL/defaultUi.frag -o ../Data/default/ShaderModule/Compiled/defaultUiFrag.spv
 pause
